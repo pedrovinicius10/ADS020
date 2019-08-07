5379312afcc7b10653ce8a8d28083c9620ec6ca8
18114290042 <18114290042@OLAB04-D14.academico.iesb.br>
2019-08-06 21:37:26 -0300

    Atividade 01

---
+++ Atividade01/src/Modelo/persistencia/DadosException.java
@@ -0,0 +1,15 @@
+
+package Modelo.persistencia;
+
+
+public class DadosException extends Exception {
+
+    public DadosException(String message) {
+        super(message);
+    }
+
+    public DadosException(Throwable cause) {
+        super(cause);
+    }
+    
+}
