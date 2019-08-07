3f481423185545e862f79e98c85b6dc4ff5f8c63
18114290042 <18114290042@OLAB04-D14.academico.iesb.br>
2019-08-06 21:47:04 -0300

    Atividade 01

---
+++ Atividade01/src/Modelo/entidades/Alunos.java
@@ -0,0 +1,37 @@
+package Modelo.entidades;
+ 
+import java.io.Serializable;
+
+
+public class Alunos implements Serializable {
+    
+    private int id;
+    private int matricula;
+    private String nome;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    
+}
